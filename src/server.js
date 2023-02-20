f7697a749a2f70709379f7d2dae8861f64ba57c7
Robert Zdeb <robinio123@gmail.com>
2023-02-20 15:02:20 +0000

    resolve gh-pages

--- src/server.js
+++ src/server.js
@@ -3,6 +3,31 @@ const express = require("express")
 
 const app = express();
 
+app.use(express.json())
+
+app.get("/home", (request, response) => {
+    // console.log(request);
+    // response.send("hello from the home page");
+    const book = {
+        title: "lotr",
+        author: "tolkien",
+        genre: "fantasy"
+    };
+
+    const successResponse = {
+        message: "response sent successfully",
+        book: book,
+    };
+
+    response.send(successResponse);
+
+
+});
+
+app.get("/movie", (request, response) => {
+    response.send("hello from the movie page");
+});
+
 app.use("/home", express.static("home"));
 app.use("/movies", express.static("movies"));
 app.use("/bikes", express.static("bikes"));
