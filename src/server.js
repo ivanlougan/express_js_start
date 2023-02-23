1453013298ec64a52b9f9ce1d0199177c105d93b
Robert Zdeb <robinio123@gmail.com>
2023-02-23 09:54:58 +0000

    crud methods added

--- src/server.js
+++ src/server.js
@@ -1,45 +1,38 @@
+require("dotenv").config();
+require("./db/connection");
 // import express from "../node_modules"
 const { response } = require("express");
 const express = require("express");
 const { request } = require("http");
 
+const Book = require("./books/model");
+
 const app = express();
 
 app.use(express.json())
 
-app.get("/home", (request, response) => {
-    // console.log(request);
-    // response.send("hello from the home page");
-    const book = {
-        title: "lotr",
-        author: "tolkien",
-        genre: "fantasy"
-    };
+app.get("/books/getallbooks", async (request, response) => {
+    
+    const allBooks = await Book.find({});
+
+    console.log(allBooks);
 
     const successResponse = {
         message: "response sent successfully",
-        book: book,
+        book: allBooks,
     };
 
     response.send(successResponse);
 });
 
 
-// {
-//     "title": "matilda",
-//     "author": "chop jakis"
-//   }
-
-app.post("/home", (request, response) => {
-    // console.log(request.body);
-    // response.send("hello from the post route");
-
-    const newBook = {
-        id: Math.floor(Math.random() * 50),
+app.post("/books/addbook", async (request, response) => {
+    
+    const newBook = await Book.create({
         title: request.body.title,
         author: request.body.author,
-    }
-
+        genre: request.body.genre,
+    });
 
 
     const successResponse = {
@@ -50,23 +43,20 @@ app.post("/home", (request, response) => {
     response.send(successResponse);
 });
 
-app.put("/home", (request, response) => {
+app.put("/books/updatebookgenre", async (request, response) => {
     // console.log(request.body);
 
-    const updatedBook = {
+    const updatedBook = await Book.updateOne({
+        title: "matilda",        
+    }, {        
+        genre: request.body.genre,
+    });
 
-        id: Math.floor(Math.random() * 50),
-        title: "lotr",
-        author: "tolkien",
-        genre: "fantasy"
-        
-    };
-
-    updatedBook["author"] = request.body.newAuthor
+    // updatedBook["genre"] = request.body.newGenre
 
     
     const successPutResponse = {
-        message: "oleole ole",
+        message: "very new book genre",
         updatedBook: updatedBook,
         
     }
@@ -74,14 +64,32 @@ app.put("/home", (request, response) => {
 
 });
 
-app.delete("/home", (request, response) => {
+app.delete("/books/deletebook", async (request, response) => {
 
-    response.send("delete an element from /home")
-})
+    const deleteBook = await Book.deleteOne({
+        title: request.body.title,
+    })
 
-app.get("/movie", (request, response) => {
-    response.send("hello from the movie page");
-    
+    const successDeleteResponse = {
+        message: "record deleted",
+        deleteBook: deleteBook,
+    }
+
+    response.send(successDeleteResponse);
+});
+
+app.delete("/books/deletebooks", async (request, response) => {
+
+    const deleteBooks = await Book.deleteMany({
+        genre: request.body.genre,
+    });
+
+    const successDeleteResponse = {
+        message: "records deleted",
+        deleteBooks: deleteBooks,
+    }
+
+    response.send(successDeleteResponse);
 });
 
 app.use("/home", express.static("home"));
